570f9d553bf92b7be01b328e924aad4dd66a67a6
corger5719-ECPI <cora.germany@gmail.com>
2026-01-29 19:46:55 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Welcome to PHP!</title>
+</head>
+
+<body>
+    <?php echo '<h1 style="text-align: center;">Welcome to PHP!</h1>'; ?>
+    <?php echo '<h2 style="text-align: center;">My Name is Cora Germany</h2>'; ?>
+    <?php echo '<p style="text-align: center;">The Date is January 29, 2026</p>'; ?>
+    <?php echo '<p style="text-align: center;">The Time is 5:33 PM</p>'; ?>
+</body>
+
+<h3>Have a Good Evening, Mr. Tran!</h3>
+</html>
